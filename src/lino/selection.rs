7b9b64be3c4307cd08b8a6787d194e8d9a992c9e
ahmednooor <m.ahmednoor7@yahoo.com>
2021-01-15 18:50:01 +0500

    0.4.6 fixed minor issue in selecting backward

--- src/lino/selection.rs
+++ src/lino/selection.rs
@@ -55,6 +55,8 @@ impl Lino {
             self.selection.end_point = self.cursor.clone();
             self.move_cursor_right();
         }
+        
+        self.last_cursor_col = self.cursor.col;
     }
 
     pub(crate) fn clear_selection(&mut self, previous_cursor: &Cursor) {
